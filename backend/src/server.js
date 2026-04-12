3d26420219f7941803f0c1bfa845bb6c26239e00
AdityaThorat <adityathorat206@gmail.com>
2026-04-12 21:55:59 +0530

    deployement config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import {ENV} from "./lib/env.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
 
-app.get("/", (req,res)=> {
-res.status(200).send("home route success!!")
+app.get("/health", (req,res)=> {
+res.status(200).send("health route!")
 })
 
+app.get("/books", (req,res)=> {
+res.status(200).send("books end point")
+})
+
+//making our app ready for deployment
+// THIS WILL SHOW THE FRONTEND VITE APP ON THE BACKEND SERVER, SHOWING THE FRONT AND AND BACKEND ON SAME SITE
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(ENV.PORT, ()=>{
     console.log(`server running at port ${ENV.PORT}`);
