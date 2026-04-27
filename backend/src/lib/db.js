addb6e5cb55ef1f1ae76aab254e2f4e9a5058260
AdityaThorat <adityathorat206@gmail.com>
2026-04-27 13:05:12 +0530

    new commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,10 +4,10 @@ import {ENV} from "./env.js"
 
 export const connectDB = async()=> {
     try{
-        await mongoose.connect(ENV.DB_URL);
-        console.log("✅ Connected to DB successfully !!")
+        const conn = await mongoose.connect(ENV.DB_URL);
+        console.log("✅ Connected to MongoDB:", conn.connection.host)
     }catch(error){
         console.error("❌Error connecting to MongoDB", error);
         process.exit(1); //0 means success,1 means failure
     }
-}
+};
