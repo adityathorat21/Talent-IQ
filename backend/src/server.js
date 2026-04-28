7e734e81323a44a6edc13cf23a7fb16746ee4a8a
AdityaThorat <adityathorat206@gmail.com>
2026-04-28 10:47:13 +0530

    initial conmmit

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ res.status(200).send("books end point")
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
     
-    app.get("/{*any}", (req,res)=>{
+    app.get("/(.*)", (req,res)=>{
         res.sendFile(path.resolve(__dirname,"../frontend", "dist", "index.html"));
     })
 }
